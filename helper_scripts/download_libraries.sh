ec4b7d3bb64a9d7e495fdde9fe09a50ce12529c3
gmike07 <mikegreenbaum1@gmail.com>
2022-08-14 09:18:42 +0000

    updated compile mahimahi bug

--- helper_scripts/download_libraries.sh
+++ helper_scripts/download_libraries.sh
@@ -1,20 +1,25 @@
 cd ..
-git branch -a
-git checkout amazon_congestion_control
+# git branch -a
+# git checkout amazon_congestion_control
 git submodule update --recursive --init
 
 sudo apt-get update -qq
 cwd=$(pwd)
 sudo apt-get install -y xmlto libboost-all-dev aptitude
 aptitude search boost
+sudo systemctl disable systemd-resolved
+sudo systemctl stop systemd-resolved
+sudo unlink /etc/resolv.conf
 sudo apt-get install -y -q gcc-7 g++-7 libmpeg2-4-dev libpq-dev \
                           libssl-dev libcrypto++-dev libyaml-cpp-dev \
                           libboost-dev liba52-dev opus-tools libopus-dev \
                           libsndfile-dev libavformat-dev libavutil-dev ffmpeg \
-                          git automake libtool python python3 cmake wget chromium-browser unzip
+                          git automake libtool python python3 cmake wget chromium-browser unzip apache2
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 99
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 99
-# sudo apt install -y mahimahi
+sudo apt install -y mahimahi
+sudo apt remove -y mahimahi
+
 git clone https://github.com/jtv/libpqxx.git
 cd libpqxx && git checkout 7.1.0 && sudo ./configure --enable-documentation=no && sudo make -j3 install
 sudo useradd --create-home --shell /bin/bash user
