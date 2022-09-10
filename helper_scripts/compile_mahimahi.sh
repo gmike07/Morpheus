4071cfea2837163f90429495b7c1a82f318c3305
gmike07 <mikegreenbaum1@gmail.com>
2022-09-10 19:14:02 +0300

    updated mahimahi compilation libraries

--- helper_scripts/compile_mahimahi.sh
+++ helper_scripts/compile_mahimahi.sh
@@ -1,6 +1,20 @@
 cd ..
 cd mahimahi
-sudo apt-get install libprotobuf-dev protobuf-compiler -y
+sudo apt-get install protobuf-compiler -y
+# download protobuf
+sudo apt-get install autoconf automake libtool curl make g++ unzip -y
+wget https://github.com/protocolbuffers/protobuf/releases/download/v3.6.1/protobuf-all-3.6.1.tar.gz
+tar -xvf protobuf-all-3.6.1.tar.gz
+rm protobuf-all-3.6.1.tar.gz
+cd protobuf-3.6.1
+sudo ./autogen.sh && sudo ./configure && sudo make
+
+make check
+sudo make install
+which protoc
+sudo ldconfig
+cd ..
+
 sudo apt install apache2-dev -y
 sudo apt install xcb -y
 sudo apt-get install libx11-dev -y
@@ -8,6 +22,7 @@ sudo apt-get install libxcb-xrm-dev -y
 sudo apt-get install libxcb-present-dev -y
 sudo apt-get install -y libsdl-pango-dev -y 
 sudo apt install git-buildpackage -y
+sudo apt autoremove
 sudo ./autogen.sh
 sudo ./configure
 cd src
